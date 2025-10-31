d0ef91dd0b2e85d613f4154768133733146fae9f
madsdr <madsdr28@gmail.com>
2025-10-31 16:19:37 +0100

    add rose-pine colors

--- lua/mads/packer.lua
+++ lua/mads/packer.lua
@@ -13,5 +13,13 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  use({
+	  'rose-pine/neovim', 
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
 end)
 
